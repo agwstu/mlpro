23d8e815ef89b474727927f36ab6fef5360a0185
agwstu <aa.wytrykowska@student.uw.edu.pl>
2021-11-26 18:15:46 +0100

    dodano dataset

--- Untitled.R
+++ Untitled.R
@@ -60,9 +60,88 @@ cmt <- function(f, n = 1000, a, b) {
 }
 
 
+# zadanie 2.16
+
+
+###(AO;  Rozwijanie  macierzy). Napisz funkcj¦ rozwin(), która przeksztaªca dan¡ macierz
+#rozmiaru n   m (niekoniecznie liczbow¡) z ustawionym atrybutem dimnames na ramk¦ danych zawieraj¡c¡
+#nm obserwacji i trzy kolumny o nazwach zadanych przy u»yciu odpowiedniego argumentu funkcji. Warto±ci
+#z macierzy maj¡ znajdowa¢ si¦ w pierwszej kolumnie, a w kolejnych dwóch   kombinacje nazw wierszy
+#i kolumn odpowiadaj¡ce podanym poziom czynnika.
+#Na przykªad obiekt WorldPhones (wbudowany) zawiera dane o liczbie telefonów (w tysi¡cach) w ró»nych
+#regionach ±wiata w wybranych
+
+
+## PADR
+## Zadanie 2.16
 
 
 
+rozwin <- function(X, name){
+  stopifnot(is.matrix(X))# is.data.frame(x)
+  stopifnot(!is.null(dimnames(X)))
+  stopifnot(is.character(name), length(name) == 3)
+  
+  res <- data.frame(as.double(X), # rozwijamy macierz
+                    # pamiętamy o układzie kolumnowym w macierzach więc:
+                    # powtarzmy każdą nazwę kolumny nrow(X) razy
+                    rep(colnames(X), each=nrow(X)),
+                    # wektor etykiet wierszy (cały) nrow(X) razy
+                    rep(rownames(X), times=ncol(X)))
+  colnames(res) <- name # ustawiamy żądane nazwy kolumn
+  return(res) # i zwracamy - wystarczy samo res
+}
 
 
+
+d <- rozwin(WorldPhones, c("ile", "gdzie", "kiedy"))
+head(d)
+
+
+
+#### UWAGA: POR. pakiet rehsape2
+# install.packages('reshape2')
+library(reshape2)
+
+
+
+?reshape2::melt
+melt(WorldPhones, varnames = c("kiedy", "gdzie"), value.name="ile")
+
+
+
+microbenchmark::microbenchmark(bazowy = rozwin(WorldPhones,
+                                               c("ile", "gdzie", "kiedy")),
+                               reshape2 = melt(WorldPhones))
+typeof(1); typeof(1L); typeof(mean); typeof(NA)
+
+
+
+> class(1); class(1L); class(mean); class(NA)
+
+
+
+> typeof(c(1L, 2L)); typeof(c(1L, 2)); typeof(c(1, "1"))
+
+
+
+ff <- factor(c("OK", "OK"), levels = c("OK", "NOK"))
+
+
+
+typeof(ff); class(ff); str(ff)
+
+attributes(ff)
+
+
+
+attr(ff, "class") <- NULL
+
+
+
+attributes(ff)
+
+
+
+typeof(ff); class(ff); str(ff)
 
