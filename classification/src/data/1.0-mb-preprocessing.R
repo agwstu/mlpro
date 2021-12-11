6fd462e601530c45c18418240279a82557b7342c
matbarPL <mateusz.baryla.1996@gmail.com>
2021-12-11 20:10:40 +0100

    Split the preprocessing into two files.

---
+++ classification/src/data/1.0-mb-preprocessing.R
@@ -0,0 +1,44 @@
+#Mateusz - Loading Data
+pacman::p_load(tidyverse)
+df <- read.csv("classification\\data\\raw\\dataset_31_credit-g.csv")
+
+#Types of variables
+features <- df %>% names()
+type <- c('qualitative', 'quantitative', 'qualitative', 'qualitative',
+          'quantitative', 'qualitative', 'qualitative', 'quantitative',
+          'qualitative', 'qualitative', 'quantitative', 'qualitative',
+          'quantitative', 'qualitative', 'qualitative', 'quantitative',
+          'qualitative', 'quantitative', 'qualitative', 'qualitative',
+          'qualitative')
+
+specific_type <- c('discrete', 'discrete', 'discrete', 'discrete',
+                   'continuous', 'discrete', 'discrete', 'continuous',
+                   'discrete', 'discrete', 'discrete', 'discrete',
+                   'discrete', 'discrete', 'discrete', 'discrete',
+                   'discrete', 'discrete', 'discrete', 'discrete',
+                   'discrete')
+
+scales <- c('ordinal', 'ordinal', 'nominal', 'nominal',
+            'ratio', 'ordinal', 'ordinal', 'ratio',
+            'nominal', 'nominal', 'ratio', 'nominal',
+            'ratio', 'nominal', 'nominal', 'ordinal',
+            'ordinal', 'discrete', 'nominal', 'nominal',
+            'nominal')
+
+pretty_names <- c('Status of existing checking account', 'Duration in month',
+                  'Credit history', 'Purpose of taking a credit', 'Credit amount',
+                  'Savings account/bonds', 'Present employment since',
+                  'Installment rate in percentage of disposable income',
+                  'Personal status and sex', 'Other debtors / guarantors',
+                  'Present residence since', 'Property',
+                  'Age in years', 'Other installment plans',
+                  'Housing', 'Number of existing credits at this bank',
+                  'Job', 'Number of people being liable to provide maintenance for',
+                  'Telephone', 'Foreign worker', 'Class of a customer')
+
+dictionary <- tibble(features = features,
+                     general_type = type,
+                     specific_type = specific_type,
+                     scale = scales,
+                     pretty_name = pretty_names)
+dictionary
--- classification/src/data/preprocessing.R
+++ classification/src/data/preprocessing.R
@@ -1,48 +1,32 @@
-pacman::p_load(tidyverse)
-df <- read.csv("classification\\data\\raw\\dataset_31_credit-g.csv")
-features <- df %>% names()
-type <- c('qualitative', 'quantitative', 'qualitative', 'qualitative',
-          'quantitative', 'qualitative', 'qualitative', 'quantitative',
-         'qualitative', 'qualitative', 'quantitative', 'qualitative',
-         'quantitative', 'qualitative', 'qualitative', 'quantitative',
-         'qualitative', 'quantitative', 'qualitative', 'qualitative',
-          'qualitative')
-specific_type <- c('discrete', 'discrete', 'discrete', 'discrete',
-                   'continuous', 'discrete', 'discrete', 'continuous',
-                   'discrete', 'discrete', 'discrete', 'discrete',
-                   'discrete', 'discrete', 'discrete', 'discrete',
-                   'discrete', 'discrete', 'discrete', 'discrete',
-                   'discrete')
-scales <- c('ordinal', 'ordinal', 'nominal', 'nominal',
-           'ratio', 'ordinal', 'ordinal', 'ratio',
-           'nominal', 'nominal', 'ratio', 'nominal',
-           'ratio', 'nominal', 'nominal', 'ordinal',
-           'ordinal', 'discrete', 'nominal', 'nominal',
-           'nominal')
-pretty_names <- c('Status of existing checking account', 'Duration in month',
-                  'Credit history', 'Purpose of taking a credit', 'Credit amount',
-                  'Savings account/bonds', 'Present employment since',
-                  'Installment rate in percentage of disposable income',
-                  'Personal status and sex', 'Other debtors / guarantors',
-                  'Present residence since', 'Property',
-                  'Age in years', 'Other installment plans',
-                  'Housing', 'Number of existing credits at this bank',
-                  'Job', 'Number of people being liable to provide maintenance for',
-                  'Telephone', 'Foreign worker', 'Class of a customer')
-dictionary <- tibble(features = features,
-                     general_type = type,
-                    specific_type = specific_type,
-                     scale = scales,
-                     pretty_name = pretty_names)
-dictionary
+#####################################
+### Classification: Preprocessing ###
+#####################################
 
-#Handling NAs
+
+###
+
+###Libraries
+#install.packages("tidyverse")
+#install.packages('ggpubr')
+#install.packages('rstatix')
+library(tidyverse)
+library(ggpubr)
+library(rstatix)
+library(compare)
+library(corrplot)
+library(Hmisc)
+library(smbinning)
+
+#Agata - Loading Data
+df <- read.csv("~/Desktop/studia/magisterka/3sem/ML2/mlpro/dataset_31_credit-g.csv")
+
+
+#Handling NAs 
 dim(df)
 table(is.na(df))
 #no NAs
 
 # Spotting variables' duplicates
-
 table(duplicated(df))
 
 # no duplicates in data
