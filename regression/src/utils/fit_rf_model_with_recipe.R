c29c6679ec5c864ef9a9941f6b3719039fbe0577
matbarPL <mateusz.baryla.1996@gmail.com>
2022-01-05 18:22:32 +0100

    Generated main regression template. Added some methods to utils.

--- regression/src/utils/fit_rf_model_with_recipe.R
+++ regression/src/utils/fit_rf_model_with_recipe.R
@@ -4,7 +4,7 @@ fit_rf_model_with_recipe <- function(recipe, data){
   rf_wflow <-
     workflow() %>%
       add_model(rf_model) %>%
-      add_recipe(simple_recipe)
+      add_recipe(recipe)
 
   rf_fit <- fit(rf_wflow,
                 data)
